03ae99dfc0263548759a6a813686790c50066cc9
Jerry Doran <jmdwebtechnologies@gmail.com>
2021-01-11 12:44:38 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Form from "./Form";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
---
+++ src/useForm.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+const useForm = (callback, validate) => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const [errors, setErrors] = useState({});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validate(values));
+    setSubmitting(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && submitting) {
+      callback();
+    }
+  }, [errors]);
+
+  return { handleChange, handleSubmit, values, errors };
+};
+
+export default useForm;
